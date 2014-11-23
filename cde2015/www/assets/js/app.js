27cf0f8520ab39132360766d84750d0213f845f7
Clarisse Schneider <clarissemschneider@gmail.com>
2014-11-22 21:35:19 -0800

    Would actually help if I pushed up the harp ejs stuff...

---
+++ cde2015/www/assets/js/app.js
@@ -0,0 +1,27 @@
+var isMobile = {
+    Android: function() {
+        return navigator.userAgent.match(/Android/i);
+    },
+    BlackBerry: function() {
+        return navigator.userAgent.match(/BlackBerry/i);
+    },
+    iOS: function() {
+        return navigator.userAgent.match(/iPhone|iPad|iPod/i);
+    },
+    Opera: function() {
+        return navigator.userAgent.match(/Opera Mini/i);
+    },
+    Windows: function() {
+        return navigator.userAgent.match(/IEMobile/i);
+    },
+    any: function() {
+        return (isMobile.Android() || isMobile.BlackBerry() || isMobile.iOS() || isMobile.Opera() || isMobile.Windows());
+    }
+};
+$(document).ready(function(){
+	if(!isMobile.any() && !window.matchMedia('(max-width: 800px)').matches) {
+		$(window).stellar({
+			 horizontalScrolling: false
+		});
+	}
+});
