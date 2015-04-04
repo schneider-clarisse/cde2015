0e08702fcdc7b1c02599ffb0d7db7369fb65222f
Clarisse Schneider <cmschnei@uwaterloo.ca>
2015-04-03 22:09:17 -0400

    Because it's quite apparent I haven't pushed in a while...

--- cde2015/www/assets/js/app.js
+++ cde2015/www/assets/js/app.js
@@ -19,9 +19,70 @@ var isMobile = {
     }
 };
 $(document).ready(function(){
-	if(!isMobile.any() && !window.matchMedia('(max-width: 800px)').matches) {
-		$(window).stellar({
-			 horizontalScrolling: false
-		});
-	}
+
+    var language = window.navigator.userLanguage || window.navigator.language;
+    var cookie = document.cookie;
+    if((document.cookie.indexOf('language') >= 0 && getCookie('language') == 'fr') || language.indexOf('fr') >= 0) {
+        $('.en').toggle();
+        $('#francais').addClass("selected");
+        $('#english').removeClass("selected");
+    } else {
+        $('.fr').toggle();
+    }
+
+    $('#english').bind('click', function() {
+        if ($('#francais').hasClass("selected")) {
+            $('#english').addClass("selected");
+            $('#francais').removeClass("selected");
+
+            $('.fr').toggle();
+            $('.en').toggle();
+
+            setCookie('language', 'en', 365);
+        }
+    });
+    $('#francais').bind('click', function() {
+        if ($('#english').hasClass("selected")) {
+            $('#francais').addClass("selected");
+            $('#english').removeClass("selected");
+
+            $('.fr').toggle();
+            $('.en').toggle();
+
+            setCookie('language', 'fr', 365);
+        }
+    });
+});
+
+function setCookie(cname, cvalue, exdays) {
+    var d = new Date();
+    d.setTime(d.getTime() + (exdays*24*60*60*1000));
+    var expires = "expires="+d.toUTCString();
+    document.cookie = cname + "=" + cvalue + "; " + expires;
+}
+
+function getCookie(cname) {
+    var name = cname + "=";
+    var ca = document.cookie.split(';');
+    for(var i=0; i<ca.length; i++) {
+        var c = ca[i];
+        while (c.charAt(0)==' ') c = c.substring(1);
+        if (c.indexOf(name) == 0) return c.substring(name.length, c.length);
+    }
+    return "";
+}
+
+$(function() {
+  $('a[href*=#]:not([href=#])').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      if (target.length) {
+        $('html,body').animate({
+          scrollTop: target.offset().top
+        }, 1000);
+        return false;
+      }
+    }
+  });
 });
